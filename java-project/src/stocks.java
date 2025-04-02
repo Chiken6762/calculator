daae6db3436fc8eadbe92c0b9413b3241b5142eb
Anshul Srivastava <anshul.2506762@gmail.com>
2025-04-03 01:27:00 +0530

    changed the folder structure

---
+++ java-project/src/stocks.java
@@ -0,0 +1,17 @@
+
+
+public class stocks {
+    public static void main(String[] args) {
+        // Example usage
+        String stockSymbol = "AAPL";
+        double stockPrice = 150.00;
+        int quantity = 10;
+
+        double totalValue = calculateTotalValue(stockPrice, quantity);
+        System.out.println("Total value of " + quantity + " shares of " + stockSymbol + ": $" + totalValue);
+    }
+
+    public static double calculateTotalValue(double price, int quantity) {
+        return price * quantity;
+    }
+}
